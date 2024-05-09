3e1be37c85db5689dbaf0f6d25a3a380f7ac80c4
Alan J Bibins <alanjbibins@gmail.com>
2024-05-09 07:54:34 +0530

    Added the lazy package manager

--- init.lua
+++ init.lua
@@ -3,9 +3,37 @@ vim.g.mapleader = " "
 --setting up pwsh as the default terminal
 
 
---options here:
+--OPTIONS HERE:
+local opt = vim.opt -- aliasing
+
+-- line number
+opt.number = true
+opt.relativenumber = true
+
+-- tabs & intendation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
 
 --setting up lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
+local plugins = {}
+local opts = {}
 
-
+require("lazy").setup(plugins, opts)
