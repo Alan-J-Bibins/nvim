c193c0acb7845b33676ef732e06ed9db0d043a36
Alan-J-Bibins <alanjbibins@gmail.com>
2024-06-19 19:54:15 +0530

    Added more parsers for treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "javascript", "python", "rust" },
+            ensure_installed = { "c", "lua", "javascript", "python", "rust", "typescript", "javascript", "css", "tsx", "html" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
