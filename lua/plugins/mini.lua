7ef88588352de824583d79a11075a77de8cde936
Alan-J-Bibins <alanjbibins@gmail.com>
2025-02-25 10:44:12 +0530

    feat: Added snacks.nvim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
     "echasnovski/mini.nvim",
     config = function()
         require("mini.surround").setup()
+        require("mini.ai").setup()
         require("mini.pairs").setup()
         require("mini.indentscope").setup({
             symbol = "|",
