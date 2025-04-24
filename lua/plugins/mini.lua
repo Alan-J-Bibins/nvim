86b25bfc6508d0276a422f55cf552ca5780a99ef
Alan-J-Bibins <alanjbibins@gmail.com>
2025-04-24 18:33:58 +0530

    feat: Got file pickers

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -8,9 +8,24 @@ return {
         --     symbol = "|",
         -- })
         require("mini.comment").setup()
-        require("mini.files").setup()
+        require("mini.files").setup({
+            config = function()
+                vim.api.nvim_create_autocmd('User', {
+                    pattern = 'MiniFilesWindowOpen',
+                    callback = function(args)
+                        local win_id = args.data.win_id
+
+                        -- Customize window-local settings
+                        vim.wo[win_id].winblend = 50
+                        local config = vim.api.nvim_win_get_config(win_id)
+                        config.border, config.title_pos = 'double', 'right'
+                        vim.api.nvim_win_set_config(win_id, config)
+                    end,
+                })
+            end
+        })
         require("mini.move").setup()
 
-        vim.keymap.set('n', '<leader>e', ':lua MiniFiles.open()<CR>', {desc = "Open mini files"})
+        vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<CR>', { desc = "Open mini files" })
     end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     {
         'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
+        branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
             local builtin = require("telescope.builtin")
