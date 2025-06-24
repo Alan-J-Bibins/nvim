25998c76ccb13af146b0157b0ca660104d95379e
Alan-J-Bibins <alanjbibins@gmail.com>
2025-06-24 23:14:18 +0530

    feat: Added some useful plugins and removed some others

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,14 +1,5 @@
 return {
     {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        build = "cd app && npm install",
-        init = function()
-            vim.g.mkdp_filetypes = { "markdown" }
-        end,
-        ft = { "markdown" },
-    },
-    {
         "OXY2DEV/markview.nvim",
         lazy = false, -- Recommended
         -- ft = "markdown" -- If you decide to lazy-load anyway
