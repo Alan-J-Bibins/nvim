03536e50c953025d3d45e347f44bec377e13ef94
Alan-J-Bibins <alanjbibins@gmail.com>
2026-04-14 11:38:56 +0530

    feat: init.lua and my regular options + keymaps

---
+++ lua/options.lua
@@ -0,0 +1,26 @@
+local opt = vim.opt -- aliasing
+
+-- line number
+opt.number = true
+opt.relativenumber = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.wrap = false
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+
+opt.scrolloff = 8
+vim.o.winborder = 'none'
+
+opt.swapfile = false
+opt.undofile = true
+
+vim.g.mkdp_browser = 'firefox'
